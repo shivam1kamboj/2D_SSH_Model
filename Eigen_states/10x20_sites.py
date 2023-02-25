466d47640c00c7ae27cabf3aef0d60ac604bfa7e
Shivam Kamboj <shivam1kamboj@gmail.com>
2023-02-24 17:23:13 -0800

    Resolved edge line plots

--- Eigen_states/10x20_sites.py
+++ Eigen_states/10x20_sites.py
@@ -2,6 +2,8 @@ from Hamiltonian.Hamiltonian import Hamiltonian_2DSSH
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use('/Users/shivamkamboj/Documents/UC_Merced/Research/2D_SSH_full_project/matplotlibrc')
+
 j0 = np.linspace(-2, 2, 50)
 t = 1
 m, n, w = 10, 10, 1  # with a STO array of size 50 X50, H-matrix would be 5000 X 5000 dimensions
@@ -18,38 +20,38 @@ Vec = VI[:, igens]
 
 # Eigenvectors on the left edge
 
-plt.figure(figsize=(7, 7))
+plt.figure()
 for i, vector in enumerate(Vec.T[:4]):
     plt.subplot(2, 2, i + 1)
     plt.imshow((abs(vector).reshape(m, 2 * m)) ** 2)
 
 plt.savefig('10x20_left_edge.pdf')
 
-plt.figure(figsize=(10, 6))
+plt.figure()
 plt.xlabel(" Lattice index ")
 plt.ylabel('$Amplitude$')
 plt.title('Left Edge Eigenstates of 10x20 STO Sites With non-zero Imaginary Eigenvalue')
 for j, vector in enumerate(Vec.T[:10]):
-    plt.plot(((abs(vector).reshape(m, 2 * m)) ** 2)[:, 0], label='Correspond to eigen_Value=' + str(EI[igens][j]))
+    plt.plot(((abs(vector).reshape(m, 2 * m)) ** 2)[:, 0], label='eigenvalue=' + str(np.round(EI[igens][j], decimals=2)))
     plt.legend(loc='best', fontsize=7)
 
 plt.savefig('10x20_left_edge_line_plot.pdf')
 
 # Eigenvectors on the right edge
 
-plt.figure(figsize=(7, 7))
+plt.figure()
 for i, vector in enumerate(Vec.T[10:14]):
     plt.subplot(3, 2, i + 1)
     plt.imshow((abs(vector).reshape(m, 2 * m)) ** 2)
 
 plt.savefig('10x20_right_edge.pdf')
 
-plt.figure(figsize=(10, 6))
+plt.figure()
 plt.xlabel(" Lattice index ")
 plt.ylabel('$Amplitude$')
 plt.title('Right Edge Eigenstates of 10x20 STO Sites With negavtive imaginary Eigenvalue')
 for j, vector in enumerate(Vec.T[10:]):
-    plt.plot(((abs(vector).reshape(m, 2 * m)) ** 2)[:, -1], label='Correspond to eigen_Value=' + str(EI[igens][10:][j]))
+    plt.plot(((abs(vector).reshape(m, 2 * m)) ** 2)[:, -1], label='eigenvalue=' + str(np.round(EI[igens][j], decimals=2)))
     plt.legend(loc='best', fontsize=7)
 
 plt.savefig('10x20_right_edge_line_plot.pdf')
--- Eigen_states/50x100_sites.py
+++ Eigen_states/50x100_sites.py
@@ -2,6 +2,8 @@ from Hamiltonian.Hamiltonian import Hamiltonian_2DSSH
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use('/Users/shivamkamboj/Documents/UC_Merced/Research/2D_SSH_full_project/matplotlibrc')
+
 j0 = np.linspace(-2, 2, 50)
 t = 1
 m, n, w = 50, 50, 1  # with a STO array of size 50 X100, H-matrix would be 5000 X 5000 dimensions
@@ -22,35 +24,35 @@ NVec = VI[:, Nigens]
 
 # Eigenvectors on the left edge
 
-plt.figure(figsize=(7, 7))
+plt.figure()
 for i, vector in enumerate(Vec.T[:4]):
     plt.subplot(3, 2, i + 1)
     plt.imshow((abs(vector).reshape(m, 2 * m)) ** 2)
 
 plt.savefig('50x100_left_edge.pdf')
 
-fig = plt.figure(figsize=(10, 6))
+fig = plt.figure()
 plt.xlabel(" Lattice index ")
 plt.ylabel('$Amplitude$')
 plt.title('Left Edge Eigenstates of 50x100 STO Sites With non-zero Imaginary Eigenvalue')
-for i, vector in enumerate(Vec.T):
+for i, vector in enumerate(Vec.T[:5]):
     plt.plot(((abs(vector).reshape(m, 2 * m)) ** 2)[:, 0])
 
 plt.savefig('50x100_left_edge_line_plot.pdf')
 
 # Eigenvectors on the right edge
-plt.figure(figsize=(7, 7))
+plt.figure()
 for i, vector in enumerate(NVec.T[:4]):
     plt.subplot(3, 2, i + 1)
     plt.imshow((abs(vector).reshape(m, 2 * m)) ** 2)
 
 plt.savefig('50x100_right_edge.pdf')
 
-fig = plt.figure(figsize=(10, 6))
+fig = plt.figure()
 plt.xlabel(" Lattice index ")
 plt.ylabel('$Amplitude$')
 plt.title('Right Edge Eigenstates of 50x100 STO Sites With non-zero Imaginary Eigenvalue')
-for i, vector in enumerate(NVec.T):
+for i, vector in enumerate(NVec.T[:5]):
     plt.plot(((abs(vector).reshape(m, 2 * m)) ** 2)[:, -1])
 
 plt.savefig('50x100_right_edge_line_plot.pdf')
